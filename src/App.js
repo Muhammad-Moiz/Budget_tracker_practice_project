02000f2c21a364832441c3312f3a196d1c2d2785
MMoiz110 <mmoiz@csquareonline.com>
2022-04-27 17:20:57 +0500

    basic UI created.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Budget from "./Components/Budget";
+import Remaining from "./Components/Remining";
+import ExpenseTotal from "./Components/ExpenseTotal";
+import ExpenseList from "./Components/ExpenseList";
+import AddExpenseForm from "./Components/AddExpenseForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="mt-3">My Budget Planner </h1>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <Budget />
+        </div>
+        <div className="col-sm">
+          <Remaining />
+        </div>
+        <div className="col-sm">
+          <ExpenseTotal />
+        </div>
+      </div>
+
+      <h3 className='mt-3'>Expenses</h3>
+      <div className='row '>
+        <div className='col-sm'>
+          <ExpenseList />
+        </div>
+      </div>
+      <h3 className='mt-3'>Add Expense</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <AddExpenseForm />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Budget.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Budget = () => {
+    return (
+        <div className="alert alert-secondary">
+            <span>
+                Budget: $200
+            </span>
+        </div>
+    )
+}
+
+export default Budget
